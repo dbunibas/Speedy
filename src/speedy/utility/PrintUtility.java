ae52b488f24d98a119b7cbaef93b610423555080
donatellosantoro <santoro.donatello@gmail.com>
2016-02-24 12:49:25 +0100

    Introduce PrintUtility to print colored message in the console

---
+++ src/speedy/utility/PrintUtility.java
@@ -0,0 +1,35 @@
+package speedy.utility;
+
+public class PrintUtility {
+
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_WHITE = "\u001B[37m";
+
+    public static void printError(String message) {
+        System.out.println(ANSI_RED + message + ANSI_RESET);
+    }
+
+    public static void printWarning(String message) {
+        System.out.println(ANSI_YELLOW + message + ANSI_RESET);
+    }
+
+    public static void printSuccess(String message) {
+        System.out.println(ANSI_GREEN + message + ANSI_RESET);
+    }
+
+    public static void printInformation(String message) {
+        System.out.println(ANSI_BLUE + message + ANSI_RESET);
+    }
+
+    public static void printMessage(String message) {
+//        System.out.println(ANSI_BLACK + message + ANSI_RESET);
+        System.out.println(message);
+    }
+}
