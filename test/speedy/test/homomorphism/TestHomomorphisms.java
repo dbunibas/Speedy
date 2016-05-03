7e7f6bdf8b9ee4ce5ebe974d9284ae23f6c2fd89
Enzo Veltri <enzo.veltri@gmail.com>
2016-05-03 20:54:05 +0200

    Add new test

--- test/speedy/test/homomorphism/TestHomomorphisms.java
+++ test/speedy/test/homomorphism/TestHomomorphisms.java
@@ -1,38 +1,65 @@
 package speedy.test.homomorphism;
 
+import java.io.File;
 import junit.framework.TestCase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import speedy.comparison.InstanceMatch;
 import speedy.comparison.operators.FindHomomorphism;
+import speedy.model.algebra.operators.ITupleIterator;
+import speedy.model.database.AttributeRef;
+import speedy.model.database.Cell;
 import speedy.model.database.IDatabase;
+import speedy.model.database.ITable;
+import speedy.model.database.IValue;
+import speedy.model.database.NullValue;
+import speedy.model.database.Tuple;
 import speedy.model.database.dbms.DBMSDB;
 import speedy.model.database.dbms.InitDBConfiguration;
 import speedy.persistence.DAODBMSDatabase;
+import speedy.persistence.DAOMainMemoryDatabase;
 import speedy.persistence.file.CSVFile;
 import speedy.utility.test.UtilityForTests;
 
 public class TestHomomorphisms extends TestCase {
-    
+
     private final static Logger logger = LoggerFactory.getLogger(TestHomomorphisms.class);
 
     private FindHomomorphism homomorphismFinder = new FindHomomorphism();
     private static String BASE_FOLDER = "/resources/homomorphism/";
+    private DAOMainMemoryDatabase dao = new DAOMainMemoryDatabase();
 
     public void test1() {
         IDatabase sourceDb = loadDatabase("01", "source");
         IDatabase destinationDb = loadDatabase("01", "destination");
         InstanceMatch result = homomorphismFinder.findHomomorphism(sourceDb, destinationDb);
         logger.info(result.toString());
-        assert(result.getNonMatchingTuples() == null);
+        assert (result.getNonMatchingTuples() == null);
     }
-    
+
     public void test2() {
         IDatabase sourceDb = loadDatabase("02", "source");
         IDatabase destinationDb = loadDatabase("02", "destination");
         InstanceMatch result = homomorphismFinder.findHomomorphism(sourceDb, destinationDb);
         logger.info(result.toString());
-        assert(result.getNonMatchingTuples() == null);
+        assert (result.getNonMatchingTuples() == null);
+    }
+
+    public void test3() {
+        String baseAbsFolder = UtilityForTests.getAbsoluteFileName(BASE_FOLDER);
+        IDatabase sourceDb = dao.loadCSVDatabase(baseAbsFolder + "s2/", ',', null);
+        IDatabase destinationDb = dao.loadCSVDatabase(baseAbsFolder + "d2/", ',', null);
+        InstanceMatch result = homomorphismFinder.findHomomorphism(sourceDb, destinationDb);
+        logger.info(result.toString());
+        assert (result.getNonMatchingTuples() == null);
+        ITable table = sourceDb.getTable("table1");
+        ITupleIterator tupleIterator = table.getTupleIterator();
+        Tuple firstTuple = tupleIterator.next();
+        AttributeRef attributeRefA = new AttributeRef("table1", "A");
+        Cell cell = firstTuple.getCell(attributeRefA);
+        assertNotNull(cell);
+        IValue value = cell.getValue();
+        assertTrue(value instanceof NullValue);
     }
 
     private IDatabase loadDatabase(String expName, String schemaName) {
