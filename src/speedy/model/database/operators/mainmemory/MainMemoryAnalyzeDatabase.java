2518e5af75c9129028346b95a39826a475b5b205
Gianni Mecca <giansalvatore.mecca@gmail.com>
2016-05-31 14:02:18 +0200

    Fixed AnalyzeDB for main memory

--- src/speedy/model/database/operators/mainmemory/MainMemoryAnalyzeDatabase.java
+++ src/speedy/model/database/operators/mainmemory/MainMemoryAnalyzeDatabase.java
@@ -3,10 +3,9 @@ package speedy.model.database.operators.mainmemory;
 import speedy.model.database.IDatabase;
 import speedy.model.database.operators.IAnalyzeDatabase;
 
-
 public class MainMemoryAnalyzeDatabase implements IAnalyzeDatabase{
 
-    public void analyze(IDatabase database) {
+    public void analyze(IDatabase database, int maxNumberOfThreads) {
     }
 
 }
